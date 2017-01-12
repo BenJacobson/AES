5e7083b339e91d60d4c6cc8b170506e5f5eaa2bb
Ben Jacobson <Ben Jacobson>
2017-01-11 23:19:55 -0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/AES.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AES.dir/main.cpp.o"
+  "AES.pdb"
+  "AES.exe"
+  "libAES.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
